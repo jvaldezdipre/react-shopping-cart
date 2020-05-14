bb44061bb6297924ebdb075edd60f483992edd14
Jeffrey Valdez <53051337+jvaldezdipre@users.noreply.github.com>
2020-05-14 16:23:50 -0400

    Finished one stretch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
@@ -18,6 +18,7 @@ function App() {
   const addItem = item => {
     // add the given item to the cart
     const addedItem = {
+      id: item.id,
       image: item.image,
       title: item.title,
       price: item.price,
@@ -26,17 +27,23 @@ function App() {
     setCart([...cart, addedItem]);
   };
 
+  const deleteItem = id => {
+    setCart(cart.filter(item => item.id !== id));
+  };
+
   return (
     <div className='App'>
       <ProductContext.Provider value={{ products, addItem }}>
         <CartContext.Provider value={cart}>
           <Navigation />
+        </CartContext.Provider>
 
-          {/* Routes */}
-          <Route exact path='/'>
-            <Products />
-          </Route>
+        {/* Routes */}
+        <Route exact path='/'>
+          <Products />
+        </Route>
 
+        <CartContext.Provider value={{ cart, deleteItem }}>
           <Route path='/cart'>
             <ShoppingCart />
           </Route>
